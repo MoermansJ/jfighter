f378ff12bc8b935e4177f2a9d8c29b3cffb72875
moermansj <jonathan.moermans2@gmail.com>
2026-07-01 15:17:35 +0200

    Add projectile shooting with Space key
    Space fires a yellow circle projectile from the player's center in the
    direction they're currently facing. Projectiles travel at 400px/s and
    are removed when they leave the world bounds.

--- core/src/main/java/be/jfighter/GameScreen.java
+++ core/src/main/java/be/jfighter/GameScreen.java
@@ -3,8 +3,11 @@ package be.jfighter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
@@ -13,13 +16,16 @@ public class GameScreen implements Screen {
     private static final float WORLD_HEIGHT = 480f;
 
     private SpriteBatch batch;
+    private ShapeRenderer shapeRenderer;
     private FitViewport viewport;
     private Player player;
     private Texture playerTexture;
+    private final Array<Projectile> projectiles = new Array<>();
 
     @Override
     public void show() {
         batch = new SpriteBatch();
+        shapeRenderer = new ShapeRenderer();
         viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT);
         player = new Player(100f, (WORLD_HEIGHT - Player.HEIGHT) / 2f);
         playerTexture = new Texture(Gdx.files.internal("player.png"));
@@ -29,9 +35,11 @@ public class GameScreen implements Screen {
     public void render(float delta) {
         handleInput(delta);
         clampPlayer();
+        updateProjectiles(delta);
 
         ScreenUtils.clear(0.08f, 0.08f, 0.12f, 1f);
         viewport.apply();
+
         batch.setProjectionMatrix(viewport.getCamera().combined);
         batch.begin();
         batch.draw(playerTexture,
@@ -43,6 +51,14 @@ public class GameScreen implements Screen {
             0, 0, playerTexture.getWidth(), playerTexture.getHeight(),
             false, false);
         batch.end();
+
+        shapeRenderer.setProjectionMatrix(viewport.getCamera().combined);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.YELLOW);
+        for (Projectile p : projectiles) {
+            shapeRenderer.circle(p.x, p.y, Projectile.RADIUS);
+        }
+        shapeRenderer.end();
     }
 
     private void handleInput(float delta) {
@@ -57,6 +73,21 @@ public class GameScreen implements Screen {
         }
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             player.rotateRight(delta);
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            float cx = player.x + Player.WIDTH / 2f;
+            float cy = player.y + Player.HEIGHT / 2f;
+            projectiles.add(new Projectile(cx, cy, player.rotation));
+        }
+    }
+
+    private void updateProjectiles(float delta) {
+        for (int i = projectiles.size - 1; i >= 0; i--) {
+            Projectile p = projectiles.get(i);
+            p.update(delta);
+            if (p.isOutOfBounds(WORLD_WIDTH, WORLD_HEIGHT)) {
+                projectiles.removeIndex(i);
+            }
         }
     }
 
@@ -83,6 +114,7 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
         batch.dispose();
+        shapeRenderer.dispose();
         playerTexture.dispose();
     }
 }
---
+++ core/src/main/java/be/jfighter/Projectile.java
@@ -0,0 +1,29 @@
+package be.jfighter;
+
+import com.badlogic.gdx.math.MathUtils;
+
+public class Projectile {
+    public static final float SPEED = 400f;
+    public static final float RADIUS = 5f;
+
+    public float x;
+    public float y;
+    private final float vx;
+    private final float vy;
+
+    public Projectile(float originX, float originY, float rotation) {
+        this.x = originX;
+        this.y = originY;
+        this.vx = MathUtils.cosDeg(rotation);
+        this.vy = MathUtils.sinDeg(rotation);
+    }
+
+    public void update(float delta) {
+        x += vx * SPEED * delta;
+        y += vy * SPEED * delta;
+    }
+
+    public boolean isOutOfBounds(float worldWidth, float worldHeight) {
+        return x < 0 || x > worldWidth || y < 0 || y > worldHeight;
+    }
+}
